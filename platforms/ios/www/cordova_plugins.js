07e8093b335d313313904e3b25ddef60c95b9032
paulklinkenberg <paul@ongevraagdadvies.nl>
2016-09-28 14:29:32 +0200

    cordova-Googlemaps-plugin test setup, for pull request https://github.com/mapsplugin/cordova-plugin-googlemaps/pull/1104: "Bugfix iOS portrait display when in landscape mode (#1104)"

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-googlemaps.cordova-plugin-googlemaps",
+        "file": "plugins/cordova-plugin-googlemaps/www/googlemaps-cdv-plugin.js",
+        "pluginId": "cordova-plugin-googlemaps",
+        "clobbers": [
+            "cordova-plugin-googlemaps"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.console",
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.logger",
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "com.googlemaps.ios": "2.0.1",
+    "cordova-plugin-googlemaps": "1.3.9",
+    "cordova-plugin-console": "1.0.4"
+};
+// BOTTOM OF METADATA
+});
